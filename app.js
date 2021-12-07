c45898ca5ea3bb8748f152b41f5d3a63cee19dd9
Illensky <alexis.laroche.02240@gmail.com>
2021-12-07 17:42:11 +0100

    exercice done

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ let tableauUn = [1, 2, 3, 4, 5];
 let tableauDeux = ["valeur1", "valeur2", "valeur3", "valeur4", "valeur5", "bidulemachin", "tatayoyo"];
 
 /* Votre code ici bas */
+
+let tableauUnDeu = tableauUn.map(item => item**item);
+let tableauUnTrois = tableauUn.map(item => item*10);
+let tableauUnQuatre = tableauUn.map(item => (item+2)/26);
+let tableauUnCinq = tableauUn.filter(item => item > 2);
+let tableauUnSix = tableauUn.filter(item => item %2 === 0);
+let tableauUnSept = tableauUn.filter(item => item*3 > 10);
+
+
+let tableauDeuxDeux = tableauDeux.map(item => item.length);
+let tableauDeuxTrois = tableauDeux.map(item => [item, item.length]);
+let tableauDeuxQuatre = tableauDeux.map(item => item + item);
+let tableauDeuxCinq = tableauDeux.filter(item => item.length > 2+2);
+let tableauDeuxSix = tableauDeux.filter(item => item.length %2 === 0);
+let tableauDeuxSept = tableauDeux.filter(item => item.length -3 > 10);
